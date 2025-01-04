826ff0426bdd27b6a7f8a647018c7f143d8113ee
AdaltonF <adaltonfilho2003@gmail.com>
2025-01-04 18:48:40 -0300

    Add Event model, admin interface, and Django configuration
    Introduced the Event model with associated fields and user relationship. Registered the model in the admin interface with a custom display configuration. Updated Django project settings, IDE configuration files, and migrations to reflect these changes.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Event
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'creation_date')
+
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField(verbose_name='Data do Evento')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'event'
+
+    def __str__(self):
+        return self.title
